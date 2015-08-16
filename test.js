1ef3cb59fcd75404659a80137944c8dfec511f05
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-16 19:12:38 -0300

    Fixed test suite.

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var isPhantom = require('./');
+var test = require('tape');
+
+test('is phantom', function(t) {
+  t.deepEqual(isPhantom(), true, 'result should be true when current running context is phantom');
+  t.end();
+});
